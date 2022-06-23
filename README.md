02e71670fe108c18b4fc22b1dd2593db1b9d61e7
hebiiro <96464759+hebiiro@users.noreply.github.com>
2022-06-23 21:53:40 +0900

    1.0.0
    初版

---
+++ README.md
@@ -0,0 +1,39 @@
+# AviUtl プラグイン - スクリプト並び替えパッチ
+
+スクリプト並び替えプラグインの不具合と競合を解消します。
+[最新バージョンをダウンロード](../../releases/latest/)
+
+- スクリプトを設定ダイアログにドラッグアンドドロップしたとき、「震える」になってしまう問題を解決します。
+	- この問題は『設定ダイアログにスクロールを付ける』＆『設定ダイアログを高速描画する』＆『スクリプト選択コンボボックスがウィンドウ外に出現する』の条件が重なったときに発生します。
+- スクリプトを設定ダイアログにドラッグアンドドロップしたあと、rikky_module などのプラグインが正常に動作しなくなる問題を解決します。
+	- rikky_module の colordialog などのボタンが作成されなくなります。
+
+## 導入方法
+
+以下のファイルを AviUtl の Plugins フォルダに入れてください。
+* script_sort_patch.auf
+
+## 更新履歴
+
+* 1.0.0 - 2022/06/24 初版
+
+## 動作確認
+
+* (必須) AviUtl 1.10 & 拡張編集 0.92 http://spring-fragrance.mints.ne.jp/aviutl/
+* (共存確認) patch.aul r41 https://scrapbox.io/ePi5131/patch.aul
+
+## クレジット
+
+* Microsoft Research Detours Package https://github.com/microsoft/Detours
+* aviutl_exedit_sdk https://github.com/ePi5131/aviutl_exedit_sdk
+* Common Library https://github.com/hebiiro/Common-Library
+
+## 作成者情報
+ 
+* 作成者 - 蛇色 (へびいろ)
+* GitHub - https://github.com/hebiiro
+* Twitter - https://twitter.com/io_hebiiro
+
+## 免責事項
+
+このプラグインおよび同梱物を使用したことによって生じたすべての障害・損害・不具合等に関しては、私と私の関係者および私の所属するいかなる団体・組織とも、一切の責任を負いません。各自の責任においてご使用ください。
---
+++ script_sort_patch/script_sort_patch.cpp
@@ -0,0 +1,187 @@
+﻿#include "pch.h"
+
+// デバッグ用コールバック関数。デバッグメッセージを出力する。
+void ___outputLog(LPCTSTR text, LPCTSTR output)
+{
+	::OutputDebugString(output);
+}
+
+AviUtlInternal g_auin;
+
+DECLARE_HOOK_PROC(LRESULT, WINAPI, SettingDialogProc, (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam));
+DECLARE_HOOK_PROC(LONG, WINAPI, SetWindowLongA, (HWND hwnd, int index, LONG newLong));
+
+HWND getComboBox(HWND settingDialog)
+{
+	int i = 0; // デバッグ用。
+	HWND child = ::GetWindow(settingDialog, GW_CHILD);
+	child = ::GetWindow(child, GW_HWNDLAST);
+	while (child)
+	{
+		UINT id = ::GetDlgCtrlID(child);
+
+		if (id >= 8100 && id <= 8147)
+		{
+			MY_TRACE(_T("%d : %d\n"), i, id);
+
+			if (::IsWindowVisible(child))
+			{
+				// コンボボックスかどうかクラス名で調べる。
+				TCHAR className[MAX_PATH] = {};
+				::GetClassName(child, className, MAX_PATH);
+				MY_TRACE_TSTR(className);
+
+				if (::lstrcmpi(className, _T("ComboBox")) == 0)
+				{
+					// 目的のコンボボックスかどうかウィンドウテキストで調べる。
+					TCHAR windowText[MAX_PATH] = {};
+					::GetWindowText(child, windowText, MAX_PATH);
+					MY_TRACE_TSTR(windowText);
+
+					if (::lstrcmp(windowText, _T("震える")) == 0)
+					{
+						return child;
+					}
+				}
+			}
+		}
+
+		i++;
+		child = ::GetWindow(child, GW_HWNDPREV);
+	}
+
+	return 0;
+}
+
+IMPLEMENT_HOOK_PROC_NULL(LRESULT, WINAPI, SettingDialogProc, (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam))
+{
+	switch (message)
+	{
+	case WM_CREATE:
+		{
+			MY_TRACE(_T("SettingDialogProc(WM_CREATE)\n"));
+
+			// script_sort.auf 内の ::SetWindowLongA() をフックする。
+
+			HMODULE module = ::GetModuleHandle(_T("script_sort.auf"));
+
+			if (module)
+			{
+				true_SetWindowLongA = hookImportFunc(module, "SetWindowLongA", hook_SetWindowLongA);
+			}
+
+			break;
+		}
+	case WM_COMMAND:
+		{
+			UINT code = HIWORD(wParam);
+			UINT id = LOWORD(wParam);
+			HWND sender = (HWND)lParam;
+
+//			MY_TRACE(_T("WM_COMMAND, 0x%04X, 0x%04X, 0x%08X)\n"), code, id, sender);
+
+			if (id == 2079)
+			{
+				MY_TRACE(_T("アニメーション効果が追加されました\n"));
+
+				// 手動でコンボボックスを探し出してから WM_CTLCOLOREDIT を送信する。
+				// ::UpdateWindow() を使う方法はコンボボックスがウィンドウ外に隠れているような状況だと効果がない。
+
+				LRESULT result = true_SettingDialogProc(hwnd, message, wParam, lParam);
+				HWND combobox = getComboBox(hwnd);
+				MY_TRACE_HEX(combobox);
+				::SendMessage(hwnd, WM_CTLCOLOREDIT, 0, (LPARAM)combobox);
+				return result;
+			}
+
+			break;
+		}
+	}
+
+	return true_SettingDialogProc(hwnd, message, wParam, lParam);
+}
+
+IMPLEMENT_HOOK_PROC_NULL(LONG, WINAPI, SetWindowLongA, (HWND hwnd, int index, LONG newLong))
+{
+	MY_TRACE(_T("SetWindowLongA(0x%08X, %d, 0x%08X)\n"), hwnd, index, newLong);
+
+	// フック前の関数を保存してフック後に復元する。
+	// script_sort.auf が他のプラグインのフックも外してしまうのを防ぐため。
+
+	if (index == GWL_WNDPROC)
+	{
+		const LPCTSTR propName = _T("script_sort_patch");
+
+		if (newLong)
+		{
+			MY_TRACE(_T("元の関数を保存します\n"));
+			LONG retValue = true_SetWindowLongA(hwnd, index, newLong);
+			::SetProp(hwnd, propName, (HANDLE)retValue);
+			return retValue;
+		}
+		else
+		{
+			MY_TRACE(_T("元の関数を復元します\n"));
+			newLong = (LONG)::GetProp(hwnd, propName);
+			::RemoveProp(hwnd, propName);
+		}
+	}
+
+	return true_SetWindowLongA(hwnd, index, newLong);
+}
+
+//---------------------------------------------------------------------
+//		フィルタ構造体のポインタを渡す関数
+//---------------------------------------------------------------------
+EXTERN_C __declspec(dllexport) FILTER_DLL* CALLBACK GetFilterTable()
+{
+	static TCHAR g_filterName[] = TEXT("スクリプト並び替えパッチ");
+	static TCHAR g_filterInformation[] = TEXT("スクリプト並び替えパッチ 1.0.0 by 蛇色");
+
+	static FILTER_DLL g_filter =
+	{
+		FILTER_FLAG_NO_CONFIG |
+		FILTER_FLAG_ALWAYS_ACTIVE |
+		FILTER_FLAG_DISP_FILTER |
+		FILTER_FLAG_EX_INFORMATION,
+		0, 0,
+		g_filterName,
+		NULL, NULL, NULL, NULL, NULL,
+		NULL, NULL, NULL,
+		NULL,
+		func_init,
+		func_exit,
+		NULL,
+		NULL,
+		NULL, NULL,
+		NULL,
+		NULL,
+		g_filterInformation,
+		NULL, NULL,
+		NULL, NULL, NULL, NULL,
+		NULL,
+	};
+
+	return &g_filter;
+}
+
+//---------------------------------------------------------------------
+//		初期化
+//---------------------------------------------------------------------
+BOOL func_init(FILTER *fp)
+{
+	if (!g_auin.initExEditAddress())
+		return FALSE;
+
+	true_SettingDialogProc = g_auin.HookSettingDialogProc(hook_SettingDialogProc);
+
+	return TRUE;
+}
+
+//---------------------------------------------------------------------
+//		終了
+//---------------------------------------------------------------------
+BOOL func_exit(FILTER *fp)
+{
+	return FALSE;
+}
